2c54feb85088007555674caebdffb41da2c6d80e
Alex Zhong <0alexzhong0@gmail.com>
2022-01-27 11:01:41 +1300

    Add NFTLowerGas contract

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
+// number is the number of files in the directory
+// layers should be 1000x1000 pixels or 1:1 asepect ratio
+
+// REMINDER: Order of the layer matters
+// TODO: refactor to read all files in the directory using the fs module
 const layersOrder = [
     { name: 'background', number: 1 },
     { name: 'ball', number: 2 },
